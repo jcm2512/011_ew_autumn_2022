1f548a7800ee632b342e33822c49f9656c73266b
jcm2512 <joe.magalotti@gmail.com>
2022-06-27 20:19:46 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<main>
+  <div id="header">Header</div>
+  <div id="footer">Footer</div>
+</main>
+
+<style>
+  main {
+    background-color: black;
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  div {
+    color: white;
+  }
+</style>
